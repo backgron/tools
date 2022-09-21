8dc10b31a43577733eaec546d69c90c82fd87835
backgron <1319536124@qq.com>
2022-09-21 21:07:56 +0800

    fix: 更新项目配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,22 @@
+let packageName: string;
+
+if (process.env.NODE_ENV_PACKAGENAME) {
+  packageName = process.env.NODE_ENV_PACKAGENAME;
+  console.log(packageName);
+} else {
+  throw Error('请选择索要打包的package，例如："pnpm run build-utils"');
+}
+
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: {},
-  pkgs: ['backgron-utils', 'backgron-ui'],
+
+  esm: {
+    input: `./packages/${packageName}/src`,
+    output: `./packages/${packageName}/dist/esm`,
+  },
+  cjs: {
+    input: `./packages/${packageName}/src`,
+    output: `./packages/${packageName}/dist/cjs`,
+  },
+  // esm: {},
 };
